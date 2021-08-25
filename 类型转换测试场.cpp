637b82783a56ac1bc7e50d1c70befb80cdb31967
RinkoTaketsuki <33001553+RinkoTaketsuki@users.noreply.github.com>
2021-08-25 10:16:59 +0800

    commit

--- 类型转换测试场.cpp
+++ 类型转换测试场.cpp
@@ -9,9 +9,19 @@ short sv = -5; unsigned short usv = 6; int iv = -7; unsigned uv = 8;
 long lv = -9; unsigned long ulv= 10; long long llv = -11; unsigned long long ullv = 12;
 float fv = -13.1; double dv = -14.2;
 
+class A
+{
+private:
+    int mem;
+public:
+    A(int n = 0): mem(n) {}
+    explicit operator int() { cerr << "trans\n"; return mem; }
+};
+
 int main()
 {
     // 使用IDE的lint工具查看变量的类型
-    function<double(long, int)> f;
+    A a(42);
+    int i = static_cast<int>(a);
     return 0;
 }
