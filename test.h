637bc50c3085292c97d2c4d53292d6197a14167a
RinkoTaketsuki <33001553+RinkoTaketsuki@users.noreply.github.com>
2021-08-27 17:26:50 +0800

    commit

---
+++ test.h
@@ -0,0 +1,44 @@
+// Memory Management
+#include <memory>
+// Stream
+#include <iostream>
+#include <fstream>
+#include <sstream>
+// Iterator
+#include <iterator>
+// Data Structure
+#include <vector>
+#include <list>
+#include <array>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <tuple>
+#include <bitset>
+// Generic Algorithm
+#include <algorithm>
+#include <numeric>
+// Callable Object
+#include <functional>
+// C Standard Library
+#include <cmath>
+#include <cstdio>
+#include <climits>
+
+template <typename T> 
+class A
+{
+public:
+    bool bitAnd(const T&);
+    T mem;
+};
+
+template <typename T>
+bool A<T>::bitAnd(const T &rhs)
+{
+    return (this->mem + rhs).size();
+}
+
+template class A<std::string>;
